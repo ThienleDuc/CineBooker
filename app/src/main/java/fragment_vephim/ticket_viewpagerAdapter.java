45d0ac277894d50d9d2627d2a2bb55196542ebcb
phancongquoc <phanquoc870@gmail.com>
2024-10-13 11:38:21 +0700

    Giải quyết xung đột và thêm các thay đổi

---
+++ app/src/main/java/fragment_vephim/ticket_viewpagerAdapter.java
@@ -0,0 +1,30 @@
+package fragment_vephim;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ticket_viewpagerAdapter extends FragmentStateAdapter {
+    public ticket_viewpagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Ticket_chuadungFragment();
+            case 1:
+                return new ticket_dadungFragment();
+            default:
+                return new Ticket_chuadungFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2; // Số lượng tab
+    }
+}
