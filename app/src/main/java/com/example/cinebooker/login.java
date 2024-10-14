b1a2c4b51bf6b04e867e36ed90b35b7b5af77f79
phancongquoc <phanquoc870@gmail.com>
2024-10-14 18:44:53 +0700

    Cập nhật mã đăng ký và đăng nhập

--- app/src/main/java/com/example/cinebooker/login.java
+++ app/src/main/java/com/example/cinebooker/login.java
@@ -1,6 +1,10 @@
 package com.example.cinebooker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,21 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Thiết lập sự kiện cho TextView đăng ký
+        TextView loginTextView = findViewById(R.id.register_lg);
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển đến Activity Register
+                Intent intent = new Intent(login.this, register.class);
+                startActivity(intent);
+            }
+        });
+        Button loginButton = findViewById(R.id.login_btn);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(login.this,MainActivity.class);
+            startActivity(intent);
         });
     }
 }
