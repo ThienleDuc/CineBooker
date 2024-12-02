150b2c0a7ad627c3d0083572f237c78b0a2d2433
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2024-12-02 23:23:37 +0700

    sua activity_main -> activity_maingiathai

--- app/src/main/java/com/example/cinebooker/TranGiaThai/Activity/MainActivity.java
+++ app/src/main/java/com/example/cinebooker/TranGiaThai/Activity/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maingiathai);
 
 
         tabLayout = findViewById(R.id.tab_layout);
